a60f58119f12ba93c1642266496d9a47854c3f8b
appbakers <greeny100@gmail.com>
2017-01-13 16:22:30 +0900

    * add clipboard to image file

---
+++ customizations/windows/init-windows.el
@@ -0,0 +1,21 @@
+(defun org-insert-image-from-clipboard ()
+ (interactive)
+ (let* ((the-dir (file-name-directory buffer-file-name))
+	(attachments-dir (concat the-dir "attachments"))
+	(png-file-name (format-time-string "%a%d%b%Y_%H%M%S.png"))
+	(png-path (concat attachments-dir "/" png-file-name))
+	(cvt-exe-path (concat user-emacs-directory "customizations/windows/CbImage2File.exe"))
+	(temp-buffer-name "CbImage2File-buffer"))
+  (call-process cvt-exe-path nil temp-buffer-name nil png-path)
+  (let ((result (with-current-buffer temp-buffer-name (buffer-string))))
+   (progn
+    (kill-buffer temp-buffer-name)
+    (if (string= result "")
+     (progn 
+      (insert (concat "[[./attachments/" png-file-name "]]"))
+      (org-display-inline-images))
+     (insert result))))))
+
+;;within org-mode, Ctrl-Shift-v key pastes current clipboard to the buffer.
+(define-key org-mode-map (kbd "C-S-v") 'org-insert-image-from-clipboard)
+
--- init.el
+++ init.el
@@ -141,3 +141,21 @@
 (load "setup-clojure.el")
 (load "setup-js.el")
 
+(add-to-list 'load-path "~/.emacs.d/customizations/windows")
+(load "init-windows.el")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(package-selected-packages
+   (quote
+    (markdown-mode tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
