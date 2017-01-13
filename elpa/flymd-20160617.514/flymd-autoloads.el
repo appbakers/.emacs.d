17a2a5c2a590e4831e001217532a52bad0b30d1a
appbakers <greeny100@gmail.com>
2017-01-13 13:41:30 +0900

    * initial again from https://github.com/flyingmachine/emacs-for-clojure/archive/book1.zip

--- elpa/flymd-20160617.514/flymd-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; flymd-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (flymd-flyit) "flymd" "flymd.el" (22647 1866 260184
-;;;;;;  380000))
-;;; Generated autoloads from flymd.el
-
-(autoload 'flymd-flyit "flymd" "\
-Enable realtime markdown preview on the current buffer.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("flymd-pkg.el") (22647 1866 262836 127000))
-
-;;;***
-
-(provide 'flymd-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flymd-autoloads.el ends here
--- elpa/htmlize-20161211.1019/htmlize-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "htmlize" "20161211.1019" "Convert buffer text and decorations to HTML." (quote nil))
